81b742885a7d50e9fb40f0de93cef1a3dfdfce64
4goUnited <4goUnited@gmail.com>
2022-06-29 23:03:27 +0500

    Wraped errors and fix Add func()

--- main.go
+++ main.go
@@ -11,50 +11,55 @@ import (
 
 type Arguments map[string]string
 
+//Used to work with user input at -item option
 type User struct {
 	Id string `json:"id"`
 	Email string `json:"email"`
 	Age int `json:"age"`
 }
 
-
 const filePerm = 0644
 
-//Open file(or create if not exists), read all that in there and output to writer
+//Open file(or create if not exists), read all and output to writer
 func List(fileName string, writer io.Writer) error {
 	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, filePerm)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open/create %v to list users: %w", fileName, err)
 	}
 	defer file.Close()
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read the contents of %v to list users: %w", fileName, err)
 	}
 
 	if _, err := writer.Write(bytes); err != nil {
-		return err
+		return fmt.Errorf("failed to write users to %T: %w", writer, err)
 	}
 
 	return nil
 }
 
 //Add item (if not exists). Main idea was not to write all readed slice to file, but only one record.
-func Add(args Arguments, userInput User, writer io.Writer) error {
+func Add(args Arguments, writer io.Writer) error {
+	var userInput User
 	if args["item"] == "" {
 		return errors.New("-item flag has to be specified")
+	} else {
+		if err := json.Unmarshal([]byte(args["item"]), &userInput); err != nil {
+			return fmt.Errorf("failed to Unmarshal -item input from cli: %w", err) 
+		}
 	}
 
 	file, err := os.OpenFile(args["fileName"], os.O_RDWR|os.O_CREATE, filePerm)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open/create %v to add user: %w", args["fileName"], err)
 	}
 	defer file.Close()
 
 	stat, err := file.Stat()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to recieve Stat value of %v: %w", args["fileName"], err) 
 	}
 
 	var users []User
@@ -65,19 +70,18 @@ func Add(args Arguments, userInput User, writer io.Writer) error {
 
 	        bytes, err := io.ReadAll(file)
 	        if err != nil {
-			return err
+			return fmt.Errorf("failed to read the contents of %v to check existing of user: %w", args["fileName"], err)
 	        }
 
 	        if err := json.Unmarshal(bytes, &users); err != nil {
-			fmt.Println("here")
-			return err
+			return fmt.Errorf("failed to unmarshal json file %v: %w", args["fileName"], err)
 	        }
 
 		//Search if user is already exists
 	        for _, u := range users {
 			if u.Id == userInput.Id {
 				if _, err := writer.Write([]byte(fmt.Sprintf("Item with id %v already exists", u.Id))); err != nil {
-					return err
+					return fmt.Errorf("failed to write to %T message about existing user: %w", writer, err)
 				}
 				return nil
 			}
@@ -86,26 +90,26 @@ func Add(args Arguments, userInput User, writer io.Writer) error {
 		//If no user exists, added it to the end of file
 		prfx = ",\n"
 		if _, err := file.Seek(-1, 2); err != nil {
-			return err
+			return fmt.Errorf("failed to seek in file %v: %w", args["fileName"], err)
 		}
 
 	}
 
 	if _, err := file.WriteString(prfx); err != nil {
-		return err
+		return fmt.Errorf("failed to write prefix %v to file %v: %w", prfx, args["fileName"], err)
 	}
 
 	enc := json.NewEncoder(file)
 	if err := enc.Encode(userInput); err != nil {
-		return err
+		return fmt.Errorf("failed to Encode userInput to json file %v: %w", args["fileName"], err)
 	}
 
 	if _, err := file.Seek(-1, 2); err != nil {
-		return err
+		return fmt.Errorf("failed to seek in file %v: %w", args["fileName"], err)
 	}
 
 	if _, err := file.WriteString("]"); err != nil {
-		return err
+		return fmt.Errorf("failed to write suffix %v to file %v: %w", "]", args["fileName"], err)
 	}
 
 	return nil
@@ -119,18 +123,18 @@ func FindById(args Arguments, writer io.Writer) error {
 
 	file, err := os.Open(args["fileName"])
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open %v for reading: %w", args["fileName"], err)
 	}
 	defer file.Close()
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read the contents of %v to find user: %w", args["fileName"], err)
 	}
 
 	var users []User
 	if err := json.Unmarshal(bytes, &users); err != nil {
-		return err
+		return fmt.Errorf("failed to unmarshal json file %v: %w", args["fileName"], err)
 	}
 
 	//Search for user and output it to writer 
@@ -138,22 +142,24 @@ func FindById(args Arguments, writer io.Writer) error {
 		if u.Id == args["id"] {
 			jsn, err := json.Marshal(u)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to marshal user %v in file %v: %w", u, args["fileName"], err)
 			}
 
 			if _, err := writer.Write([]byte(jsn)); err != nil {
-				return err
+				return fmt.Errorf("failed to write to %T needed user %v: %w", writer, u, err)
 			}
 			return nil
 		}
 	}
 
 	//If we don'f find User with needed ID, just output "" to writer 
-	file.Write([]byte(""))
+	if _, err := writer.Write([]byte("")); err != nil {
+		return fmt.Errorf("failed to write blank line to %T: %w", writer, err) 
+	}
 	return nil
 }
 
-//If finded - remove user from byte slice and then write to truncated (to 0) file
+//If finded - remove user from byte slice and then write to truncated (0 size) file
 func Remove(args Arguments, writer io.Writer) error {
 	if args["id"] == "" {
 		return errors.New("-id flag has to be specified")
@@ -161,18 +167,18 @@ func Remove(args Arguments, writer io.Writer) error {
 
 	file, err := os.OpenFile(args["fileName"], os.O_RDWR, filePerm)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open file %v to remove user: %w", args["fileName"], err)
 	}
 	defer file.Close()
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read the contents of %v to remove user: %w", args["fileName"], err)
 	}
 
 	var users []User
 	if err := json.Unmarshal(bytes, &users); err != nil {
-		return err
+		return fmt.Errorf("failed to unmarshal json file %v: %w", args["fileName"], err)
 	}
 
 	for i, u := range users {
@@ -181,34 +187,26 @@ func Remove(args Arguments, writer io.Writer) error {
 
 			jsn, err := json.Marshal(users)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to marshal users %v before writing to file %v: %w", u, args["fileName"], err)
 			}
 
 			file.Truncate(0)
 			file.Seek(0, 0)
 			_, err = file.Write([]byte(jsn))
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to rewrite file %v after deleting user %v: %w", args["fileName"], u, err)
 			}
 			return nil
 		}
 	}
 
 	if _, err := writer.Write([]byte(fmt.Sprintf("Item with id %v not found", args["id"]))); err != nil {
-		return err
+		return fmt.Errorf("failed to output message to writer %T: %w", writer, err)
 	}
 	return nil
 }
 
 func Perform(args Arguments, writer io.Writer) error {
-	// Variable for input item User
-	var userInput User
-	if args["item"] != "" {
-		if err := json.Unmarshal([]byte(args["item"]), &userInput); err != nil {
-			return err
-		}
-	}
-
 	if args["fileName"] == "" {
 		return errors.New("-fileName flag has to be specified")
 	}
@@ -220,7 +218,7 @@ func Perform(args Arguments, writer io.Writer) error {
 			err := List(args["fileName"], writer)
 			return err
 		case "add":
-			err := Add(args, userInput, writer)
+			err := Add(args, writer)
 			return err
 		case "findById":
 			err := FindById(args, writer)
